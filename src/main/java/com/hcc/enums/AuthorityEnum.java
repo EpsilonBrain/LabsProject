8f4e7da1934ba3c0bcf29ff60c4d7f58e3833a7f
Tom Tarpey <tom.tarpey@lambdaschool.com>
2023-10-22 22:56:37 +0100

    Revert "Ticket1 dev"

--- src/main/java/com/hcc/enums/AuthorityEnum.java
+++
@@ -1,7 +0,0 @@
-package com.hcc.enums;
-
-public enum AuthorityEnum {
-    ROLE_LEARNER,
-    ROLE_REVIEWER,
-    ROLE_ADMIN;
-}
--- src/main/java/com/hcc/repositories/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hcc.repositories;
-
-import com.hcc.entities.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-}
